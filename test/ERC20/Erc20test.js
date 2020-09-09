0b0e6e583a8611b0bf97baaf40bf88402773855b
jiaqinggang1 <forestjqg@126.com>
2020-09-08 23:00:55 -0700

    fixed test error

--- test/ERC20/Erc20test.js
+++ test/ERC20/Erc20test.js
@@ -13,7 +13,7 @@ contract('Erc20Proxy', function (accounts){
         let decimals = await erc20.decimals();
         assert.equal(decimals,18,"decimals Error");
         let totalSupply = await erc20.totalSupply();
-        let total = "40000000000000000000000000000000000000000";
+        let total = "10000000000000000000000000000000";
         assert.equal(totalSupply.toString(10),total,"totalSupply Error");
         await erc20.approve(accounts[2],10000000000);
         let result = await erc20.allowance(accounts[0],accounts[2]);
@@ -22,14 +22,14 @@ contract('Erc20Proxy', function (accounts){
         result = await erc20.allowance(accounts[0],accounts[2]);
         console.log(result.toString(10))
         result = await erc20.balanceOf(accounts[0]);
-        assert.equal(result.toString(10),"9999999999999999999999999999990000000000","accounts[0] Error");
+        assert.equal(result.toString(10),"999999999999999999990000000000","accounts[0] Error");
         result = await erc20.balanceOf(accounts[3]);
         assert.equal(result.toString(10),"10000000000","accounts[3] Error");
         result = await erc20.balanceOf(accounts[2]);
         assert.equal(result.toString(10),"0","accounts[2] Error");
         await erc20.transfer(accounts[4],10000000000);
         result = await erc20.balanceOf(accounts[0]);
-        assert.equal(result.toString(10),"9999999999999999999999999999980000000000","accounts[0] Error");
+        assert.equal(result.toString(10),"999999999999999999980000000000","accounts[0] Error");
         result = await erc20.balanceOf(accounts[3]);
         assert.equal(result.toString(10),"10000000000","accounts[3] Error");
         result = await erc20.balanceOf(accounts[2]);
