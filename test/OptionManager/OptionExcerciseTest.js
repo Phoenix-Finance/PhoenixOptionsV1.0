d7e3833b1032f6fa8fd32eaf2006136244bc7b52
jiaqinggang1 <forestjqg@126.com>
2020-10-28 02:05:12 -0700

    added usdc test

--- test/OptionManager/OptionExcerciseTest.js
+++ test/OptionManager/OptionExcerciseTest.js
@@ -9,16 +9,22 @@ let PRICETWO = 2e8;
 let PRICETHREE = 3e8;
 let PRICEFOUR = 4e8;
 let SHIFTVALUE = 5e7;
+
+let OPTION_UP = 0;
+let OPTION_DOWN = 1;
+
+let ETH_ID = 2;
+let BTC_ID = 1;
+
 contract('OptionsManagerV2', function (accounts) {
     let contracts;
-    let ethAmount = new BN("10000000000000000000");
-    let usdcAmount = 10000000000;
+    let collAmount = new BN("1000000000000000000000000",10);
+    let payamount = new BN("1000000000000000");
+    let optamount = new BN("100000000000");
+    let days = 24*60*60;
+    let expiration = [days,2*days,3*days, 7*days, 10*days, 15*days,20*days, 30*days];
 
-    let payamount = new BN("100000000000000000000");
-    let optamount = new BN("2000000000000000000");
-
-    let FNXAmount =  new BN("100000000000000000000");;
-
+    let optionid = 0;
     before(async () => {
         contracts = await migration(accounts);
         await AddCollateral0(contracts);
@@ -27,22 +33,41 @@ contract('OptionsManagerV2', function (accounts) {
     });
 
     it('FNX buy and exercise', async function () {
-        let collAmount = new BN("1000000000000000000000000",10);
+
         await contracts.FNX.approve(contracts.manager.address,collAmount);
         await contracts.manager.addCollateral(contracts.FNX.address,collAmount);
-        let days = 24*60*60;
-        let expiration = [days,2*days,3*days, 7*days, 10*days, 15*days,20*days, 30*days];
         contracts.oracle.setFakeUnderlyingPrice(PRICETWO);
         let i =0 ;
-        await contracts.FNX.approve(contracts.manager.address,2000000000000000);
+        await contracts.FNX.approve(contracts.manager.address,payamount);
         let strikePrice = PRICETWO;
-        let tx = await contracts.manager.buyOption(contracts.FNX.address,1000000000000000,strikePrice,1,expiration[0],100000000000,1);
+        let tx = await contracts.manager.buyOption(contracts.FNX.address,payamount,strikePrice,ETH_ID,expiration[0],optamount,OPTION_DOWN);
         assert.equal(tx.receipt.status,true);
-
+        optionid++;
 
         contracts.oracle.setFakeUnderlyingPrice(PRICEONE);
         contracts.price.setOptionsPrice(PRICEONE + SHIFTVALUE);
-        tx = await contracts.manager.exerciseOption(1,100000000000);
+        tx = await contracts.manager.exerciseOption(optionid,optamount);
         assert.equal(tx.receipt.status,true);
+
     })
+
+    it('USDC buy and exercise', async function () {
+        await contracts.USDC.approve(contracts.manager.address,collAmount);
+        await contracts.manager.addCollateral(contracts.USDC.address,collAmount);
+
+        contracts.oracle.setFakeUnderlyingPrice(PRICETWO);
+        await contracts.USDC.approve(contracts.manager.address,payamount);
+        let strikePrice = PRICETWO;
+        let tx = await contracts.manager.buyOption(contracts.USDC.address,payamount,strikePrice,1,expiration[0],optamount,OPTION_DOWN);
+        assert.equal(tx.receipt.status,true);
+        optionid++;
+
+        contracts.oracle.setFakeUnderlyingPrice(PRICEONE);
+        contracts.price.setOptionsPrice(PRICEONE + SHIFTVALUE);
+        tx = await contracts.manager.exerciseOption(optionid,optamount);
+        assert.equal(tx.receipt.status,true);
+
+    })
+
+
 })
